d97c90cfc39c8f81f552c3a5e897127bacb825b5
Léo Torrez <leotorrez@MacBook-Air-de-Valdilene.local>
2020-02-15 08:23:31 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InputFieldComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/input-field/input-field.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-field',
+  templateUrl: './input-field.component.html',
+  styleUrls: ['./input-field.component.scss']
+})
+export class InputFieldComponent implements OnInit {
+  @Input()
+  placeholder: string;
+  @Input()
+  type: string;
+  @Input()
+  login = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
